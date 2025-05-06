ac62aae3951c725fac6bf138a1972180e3c27d00
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2025-05-06 14:39:34 +0100

    feat: perf improvements via multiprocessing. * dropped stdout mode for file-only. * defaults to using a worker pool sized according to your HW threads.

--- paygen/main.py
+++ paygen/main.py
@@ -6,46 +6,34 @@ from paygen import gen
 app = typer.Typer()
 
 @app.command()
-def binary(samples: int, min_size: int = 100, max_size: int = 1000000, write_files: bool = False):
+def binary(samples: int, min_size: int = 100, max_size: int = 1000000):
     """Generate random binary payloads following a power law distribution"""
     payloads = gen.binary_payloads(samples, min_size, max_size)
     
-    if write_files:
-        for payload in payloads:
-            filename = f"{uuid.uuid4().hex[:10]}.bin"
-            with open(filename, 'wb') as f:
-                f.write(payload)
-    else:
-        for payload in payloads:
-            print(payload)
+    for payload in payloads:
+        filename = f"{uuid.uuid4().hex[:10]}.bin"
+        with open(filename, 'wb') as f:
+            f.write(payload)
 
 @app.command()
-def json(samples: int, min_size: int = 100, max_size: int = 1000000, write_files: bool = False):
+def json(samples: int, min_size: int = 100, max_size: int = 1000000):
     """Generate random JSON payloads following a power law distribution"""
     payloads = gen.json_payloads(samples, min_size, max_size)
     
-    if write_files:
-        for payload in payloads:
-            filename = f"{uuid.uuid4().hex[:10]}.json"
-            with open(filename, 'wb') as f:
-                f.write(payload)
-    else:
-        for payload in payloads:
-            print(payload)
+    for payload in payloads:
+        filename = f"{uuid.uuid4().hex[:10]}.json"
+        with open(filename, 'wb') as f:
+            f.write(payload)
 
 @app.command()
-def text(samples: int, min_size: int = 100, max_size: int = 1000000, write_files: bool = False):
+def text(samples: int, min_size: int = 100, max_size: int = 1000000):
     """Generate random text payloads following a power law distribution"""
     payloads = gen.text_payloads(samples, min_size, max_size)
     
-    if write_files:
-        for payload in payloads:
-            filename = f"{uuid.uuid4().hex[:10]}.txt"
-            with open(filename, 'wb') as f:
-                f.write(payload)
-    else:
-        for payload in payloads:
-            print(payload)
+    for payload in payloads:
+        filename = f"{uuid.uuid4().hex[:10]}.txt"
+        with open(filename, 'wb') as f:
+            f.write(payload)
 
 
 if __name__ == "__main__":
